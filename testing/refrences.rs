85b26c0939fb53cbab287caa2bba319d5b96c71d
DarshPareek <pareekdarsh0162@gmail.com>
2023-08-21 23:13:15 +0530

    Learning Rust

---
+++ testing/refrences.rs
@@ -0,0 +1,17 @@
+fn main() {
+    let mut x: i32 = 10; //Normal Integer
+    let ref_x: &mut i32 = &mut x; //A mutable refrence to a mutable integer
+    let mut y: i32 = *ref_x;
+    y += 100;
+    *ref_x = 100;
+    println!("Refrence to an Integer");
+    println!("x: {y}");
+    println!("x: {x}");
+    let mut a: [i32; 6] = [10, 20, 30, 40, 50, 60];
+    println!("a: {a:?}");
+    let s: &[i32] = &a[2..4];
+    //reading
+    let val: i32 = s[1];
+    println!("s: {s:?}");
+    println!("val: {val:?}");
+}
