420c5a94df5b29acf3a0c36b0f58037beb71cc51
DarshPareek <pareekdarsh0162@gmail.com>
2023-09-05 16:41:40 +0530

    learning rust

---
+++ simple_gui/target/debug/deps/simple_gui.d
@@ -0,0 +1,5 @@
+D:\Github\ComprehensiveRust\simple_gui\target\debug\deps\simple_gui.exe: src\main.rs
+
+D:\Github\ComprehensiveRust\simple_gui\target\debug\deps\simple_gui.d: src\main.rs
+
+src\main.rs:
---
+++ simple_gui/target/debug/simple_gui.d
@@ -0,0 +1,1 @@
+D:\Github\ComprehensiveRust\simple_gui\target\debug\simple_gui.exe: D:\Github\ComprehensiveRust\simple_gui\src\main.rs
